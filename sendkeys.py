0acc2f4ce5377f076527ac744901c208ebcea196
= <=>
2020-04-23 12:25:06 -0400

    Mailgun features

--- sendkeys.py
+++ sendkeys.py
@@ -1,31 +1,32 @@
-import smtplib, ssl
 import csv
+import requests
 
-port = 465  # For SSL
-password = "Dipole2001!"
+def send_key(api_key, email_address, name, key):
+    return requests.post(
+        "https://api.mailgun.net/v3/reeve.tech/messages",
+        auth=("api", api_key),
+        data={"from": "ECCC AGM Voting Services <votebot@reeve.tech>",
+              "to": [name, email_address],
+              "subject": "Hello",
+              "html": "<h1>Hello " + name + "!</h1>" +
+              "<p>Your ECCC secret voter login access code is:</p>" +
+              "<h2>" + key + "</h2>" +
+              "<p>You can sign in using your access code <a href='https://reeve.tech'>here</a> whe the voting session is ready.<p>" +
+              "<p>Blessings,</p>" +
+              "<p>ECCC Voting Bot</p>"
+        })
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+key_file = open("mailgun.key")
+mailgun_key = [x.strip() for x in key_file.readlines()][0]
+key_file.close()
 
-with smtplib.SMTP_SSL("mail.dipole.app", port, context=context) as server:
-    server.login("contact@dipole.app", password)
+print(mailgun_key)
 
-    voters = []
-    with open("voter_data.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            voters.append(row)
+voters = []
+with open("voter_data.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        voters.append(row)
 
-    for voter in voters:
-        print(voter)
-        message =  "Subject: Your ECCC Voting Code\n"
-        message += "From: ECCC Voting Bot <votebot@covchurch.ca>\n"
-        message += "Content-type: text/html\r\n\n"
-        message += "<h1>Hello " + voter[0] + " " + voter[1] + "!</h1>"
-        message += "<p>Your ECCC secret voter login access code is:</p>"
-        message += "<h2>" + voter[3] + "</h2>"
-        message += "<p>You can log in using your access code <a href='http://192.168.2.95/ECCCVoteBotClient/'>here</a>.<p>"
-        message += "<p>Blessings,</p>"
-        message += "<p>ECCC Voting Bot</p>"
-
-        server.sendmail("contact@dipole.app", voter[2], message)
+for voter in voters:
+    print(send_key(mailgun_key, voter[2], voter[0] + " " + voter[1], voter[3]))
